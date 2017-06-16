f27fa97957871b517156876f65d3c0842f6fc457
Karol Wyskocki <karol.wyskocki@vp.pl>
2017-06-16 17:52:08 +0200

    Przeniesienie obsługi uartu

---
+++ uart.h
@@ -0,0 +1,29 @@
+/*
+ * uart.h
+ *
+ * Created: 16-06-2017 16:23:36
+ *  Author: Karol
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdbool.h>
+#include <avr/io.h>
+
+
+
+void usartInit(void);
+void setRxBuffer(char* buffer, uint16_t len);
+
+bool uartTxIsBusy(void);
+
+void sendLine(char *text);
+void sendData(char *text);
+
+bool recivedNewLine(void);
+
+
+
+#endif /* UART_H_ */
